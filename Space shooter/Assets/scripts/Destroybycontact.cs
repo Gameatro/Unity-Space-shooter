07f3a7c57e80d87409997b9ffe5b4fd60c32cab2
Gameatro <32991453+Gameatro@users.noreply.github.com>
2018-09-08 10:11:38 +0530

    First Commit
    Unity tutorial game

---
+++ Space shooter/Assets/scripts/Destroybycontact.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroybycontact : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject player;
+	public int ScoreValue;
+	private GameConstroller gameController;
+	void Start()
+	{
+		GameObject gamecontrollerobject = GameObject.FindWithTag ("GameController");
+		if (gamecontrollerobject != null) {
+			gameController = gamecontrollerobject.GetComponent<GameConstroller> ();
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Boundary")
+			return;
+		Instantiate (explosion, transform.position, transform.rotation);
+		if(other.tag=="Player")
+			
+			Instantiate (player, other.transform.position,other.transform.rotation);
+		gameController.AddScore (ScoreValue);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+		
+
+}
+}
---
+++ Space shooter/Assets/scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Boundary
+{
+	public float xmin,xmax,zmin,zmax;
+}
+public class PlayerController : MonoBehaviour{
+	public float speed;
+	private Rigidbody rb;
+	public Boundary boundary;
+	public float tilt;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+	public AudioSource audio;
+	void Update()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			audio.Play();
+		}
+	}
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		audio = GetComponent<AudioSource> ();
+	}
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity  =  movement*speed;
+		rb.position = new Vector3(
+			Mathf.Clamp(rb.position.x,boundary.xmin,boundary.xmax),
+			0.0f,
+			Mathf.Clamp(rb.position.z,boundary.zmin,boundary.zmax)
+
+		);
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f,rb.velocity.x * -tilt);
+}
+
+}
